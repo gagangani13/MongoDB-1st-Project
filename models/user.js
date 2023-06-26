d9eed214c7dc0088dc19818c1d14f154d748c470
gagangani13 <gagangani17@gmail.com>
2023-06-26 16:59:25 +0530

    Adding to cart

--- models/user.js
+++ models/user.js
@@ -1,24 +1,42 @@
-const mongodb=require('mongodb');
-const { getDb } = require('../util/database');
-
-class User{
-  constructor(name,email,id){
-    this.name=name,
-    this.email=email,
-    this._id=new mongodb.ObjectId(id)
-
+const mongodb = require("mongodb");
+const { getDb } = require("../util/database");
+const ObjectId=mongodb.ObjectId
+class User {
+  constructor(name, email,cart,id) {
+    this.name = name;
+    this.email = email;
+    this.cart=cart;
+    this._id=id
   }
-  save(){
-    const db=getDb()
-    return db.connection("users").insertOne(this)
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
   }
-  static findById(userId){
-    const db=getDb()
-    return db.connection("users").find({_id:this._id}).next()
-  
 
+  addToCart(product){
+    const cart=[...this.cart];
+    const cartProductIndex=cart.findIndex(cpi=>{
+        return cpi.productId.toString()===product._id.toString()
+      })   
+    if (cartProductIndex>=0) {
+      cart[cartProductIndex].qty+=1
+    }else{
+      cart.push({productId:new ObjectId(product._id),qty:1})
+    }
+    const updateCart=cart
+    const db = getDb();
+    return db.collection('users').updateOne({_id:new ObjectId(this._id)},{$set:{cart:updateCart}})
   }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .then((user) => user)
+      .catch((err) => console.log(err));
+  }
+
 }
-
 
 module.exports = User;
