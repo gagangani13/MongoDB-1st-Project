c374bcd6ba6cd9da2771917862a8ff0501c6b111
gagangani13 <gagangani17@gmail.com>
2023-06-27 14:14:09 +0530

    User table

--- app.js
+++ app.js
@@ -13,19 +13,19 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('649942f046dab2584177c6e6')
-//     .then(user => {
-//       req.user = new User(user.name,user.email)
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('649942f046dab2584177c6e6')
+    .then(user => {
+      req.user =user
+      next();
+    })
+    .catch(err => console.error(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
